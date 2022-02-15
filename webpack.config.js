1bb894a76a5d89280fe53fb6fac5a71a67024d07
tatiana <prasolova_13@mail.ru>
2022-02-15 20:17:48 +0600

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+  
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './public/index.html',
+      }),
+  ], 
+};
